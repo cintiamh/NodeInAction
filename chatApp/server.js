034622377c0f0da24b4ef35451b6074cd53eb8b2
Cintia Higashi <cintiamh@gmail.com>
2017-06-13 10:08:14 -0700

    Creating the structure

---
+++ chatApp/server.js
@@ -0,0 +1,40 @@
+var http = require('http'); // HTTP server and client functionality
+var fs = require('fs'); // Filesystem related functionality
+var path = require('path'); // Filesystem path related functionality
+var mime = require('mime'); // Derive MIME type by file extension
+var cache = {};
+
+function send404(response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: resource not found.');
+  response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+  response.writeHead(
+    200,
+    {"content-type": mime.lookup(path.basename(filePath))}
+  );
+  response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+  if (cache[absPath]) {
+    sendFile(response, absPath, cache[absPath]);
+  } else {
+    fs.exists(absPath, function(exists) {
+      if (exists) {
+        fs.readFile(absPath, function(err, data) {
+          if (err) {
+            send404(response);
+          } else {
+            cache[absPath] = data;
+            sendFile(response, absPath, data);
+          }
+        });
+      } else {
+        send404(response);
+      }
+    });
+  }
+}
